59ac23b84578bdbfeca15ba85a28a74b0b0c7ab1
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-08-14 21:27:58 -0400

    Update rolling-update.sh

--- Rolling-Update-Deployment-Strategy/rolling-update.sh
+++ Rolling-Update-Deployment-Strategy/rolling-update.sh
@@ -4,7 +4,13 @@
 # It will gradually replace the old NGINX pods with pods running the new version, 
 # ensuring that your application remains available throughout the update process.
 
-kubectl apply -f nginx-deployment.yml -n josh
+# The first version of the application 
+
+kubectl apply -f app-v1.yml -n josh
+
+# The second and the updated version of the application 
+
+kubectl apply -f app-v2.yml -n josh
 
 # You can monitor the rolling update progress using the following command: 
 
